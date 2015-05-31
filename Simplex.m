87f91ec8d5bcf1e0c97780e6822d45ecd7b11f54
yoshiomori <yoshiomori90@gmail.com>
2015-05-31 13:42:58 -0300

    Concertando Erro
    A variável b estava representado os índices básicos e  o vetor b do
    método simplex.
    Agora B representa os índices básicos, b representa o vetor b do método
    simplex e N os índices não básicos.

--- Simplex.m
+++ Simplex.m
@@ -2,20 +2,20 @@ function [ind v] = simplex(A, b, c, m, n, x)
  disp ("Simplex: Fase 2")
  nIt = 0;
  # Selecionando os indicies básicos e não básicos
- b = find (x != 0);
- nb = find (x == 0);
- invB = A(1:m, b) \ eye (m);
+ B = find (x != 0);
+ N = find (x == 0);
+ invB = A(1:m, B) \ eye (m);
  # Fazendo as iterações do método simplex
  while (1)
   disp (cstrcat ("Iterando ", num2str (nIt++)));
-  disp ([ b, x(b) ]);
+  disp ([ B, x(B) ]);
   disp (cstrcat ("Valor da função objetivo: ", num2str (c' * x)));
   # Procurando algum custo reduzido negativo
-  p = c(b)' * invB;
-  cr = c(nb) - (p * A(1:m, nb))';
+  p = c(B)' * invB;
+  cr = c(N) - (p * A(1:m, N))';
   disp ("Custos reduzidos");
-  disp ([ nb, cr ]);
-  j = nb(find (cr < 0, 1));
+  disp ([ N, cr ]);
+  j = N(find (cr < 0, 1));
   if isempty (j)
    # Não foi encontrado custo reduzido negativo para um índice não básico, terminando o algoritmo
    ind = 0;
@@ -28,26 +28,26 @@ function [ind v] = simplex(A, b, c, m, n, x)
   # Foi encontrado custo reduzido negativo para um índice não básico, calculando o u, inverso da direção básica.
   u = invB * A(1:m, j);
   disp ("Direção");
-  disp ([ b, -u ]);
+  disp ([ B, -u ]);
   if max (u) <= 0
    # Nenhuma componete de u é positiva, terminando o algoritmo
    ind = -1;
    v = zeros (n, 1);
-   v(b) = u;
+   v(B) = u;
    disp ("Problema é ilimitado com custo menos infinito:");
    disp (v);
    break;
   endif
   # Uma componente positiva de u foi encontrada, calculando theta mínimo e l, índice da componete de u que foi usado no calculo do theta mínimo.
   l = find(u>0);
-  [ thetamin, i ] = min (x(b(l))./u(l));
+  [ thetamin, i ] = min (x(B(l))./u(l));
   l = l(i);
   disp ("Theta*");
   disp (thetamin);
-  disp (cstrcat ("Sai da base: ", num2str(b(l))));
-  [ x(b(l)), b(l), nb(find(nb == j)) ] = deal (0, j, b(l));
+  disp (cstrcat ("Sai da base: ", num2str(B(l))));
+  [ x(B(l)), B(l), N(find(N == j)) ] = deal (0, j, B(l));
   x(j) = thetamin;
-  x(b(1:m!=l)) -= thetamin * u(1:m!=l);
+  x(B(1:m!=l)) -= thetamin * u(1:m!=l);
   # Calculando a invB da próxima iteração
   I = eye(m);
   I(1:m,l) = u / -u(l);
